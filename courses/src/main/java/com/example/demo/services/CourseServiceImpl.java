6c23d0f81e7bd86dfe8f555db0b7725bb475a9e0
Nishant Kumar Singh <52786028+nishu9162306344@users.noreply.github.com>
2022-04-20 16:13:15 +0530

    complete

---
+++ courses/src/main/java/com/example/demo/services/CourseServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.demo.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Course;
+@Service
+public class CourseServiceImpl implements CourseServices {
+
+List<Course> list;
+public CourseServiceImpl() {
+	
+	
+	list=new ArrayList<>();
+	list.add(new Course(145,"Java basic","Java Basic brush up...."));
+	list.add(new Course(146,"Python basic","Python Basic brush up...."));
+
+	
+}
+	
+	@Override
+	public List<Course> getCourse() {
+		// TODO Auto-generated method stub
+		return list;
+	}
+
+	@Override
+	public Course getCourses(long courseId) {
+		// TODO Auto-generated method stub
+		Course c=null;
+		for(Course course:list) {
+			if(course.getId()==courseId) {
+				
+				c=course;
+				break;
+				
+			}
+		}
+		return c;
+	}
+
+	@Override
+	public Course addcourse(Course course) {
+		// TODO Auto-generated method stub
+		list.add(course);
+		
+		return course;
+	}
+
+}
